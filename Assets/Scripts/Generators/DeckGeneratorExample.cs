56072e1527fc75ccf345f3533e4cabcad6d982e7
roman.karimov@finch-vr.com <roman.karimov@finch-vr.com>
2022-06-20 19:08:02 +0500

    example generation only deck

---
+++ Assets/Scripts/Generators/DeckGeneratorExample.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckGeneratorExample : AbstractGenerator
+{
+    public override List<AbstractCard> GenerateDeck()
+    {
+        return new List<AbstractCard>()
+        {
+            new EffectCard("FireBall", "All will be damaged", new CardChooser(ChooseType.All, true), new RandomDamage())
+        };
+    }
+
+    public override List<AbstractCard> GenerateMapCards()
+    {
+        return new List<AbstractCard>();
+    }
+}
