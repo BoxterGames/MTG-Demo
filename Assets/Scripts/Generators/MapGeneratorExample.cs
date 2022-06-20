ec10678a9b21e063102f84ec115cedf07b75b2a0
roman.karimov@finch-vr.com <roman.karimov@finch-vr.com>
2022-06-20 20:15:17 +0500

    add card generators summary

--- Assets/Scripts/Generators/MapGeneratorExample.cs
+++ Assets/Scripts/Generators/MapGeneratorExample.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// Generate player deck
+/// Generator for dummy player
 /// </summary>
 public class MapGeneratorExample : AbstractGenerator
 {
