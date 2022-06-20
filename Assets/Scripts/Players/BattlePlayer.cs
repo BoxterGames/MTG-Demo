6ca7200d21b1ce0143522568918b10e6a4bfd5ba
roman.karimov@finch-vr.com <roman.karimov@finch-vr.com>
2022-06-20 20:01:16 +0500

    base player summary

--- Assets/Scripts/Players/BattlePlayer.cs
+++ Assets/Scripts/Players/BattlePlayer.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+/// <summary>
+/// Base player who will be make turns.
+/// </summary>
 public abstract class BattlePlayer : MonoBehaviour
 {
+    /// <summary>
+    /// Player IDs
+    /// </summary>
     public int PlayerId;
+
+    /// <summary>
+    /// Call when player choosed the card/
+    /// </summary>
     public Action<AbstractCard> OnCardChoose;
 
+    /// <summary>
+    /// Card in players hand
+    /// TODO: May be set public? In this case visualizers can get info about deck, also some achievments can be create. Like: get 3 archers
+    /// </summary>
     protected List<AbstractCard> playerDeck;
 
     public virtual void Init(List<AbstractCard> deck)
