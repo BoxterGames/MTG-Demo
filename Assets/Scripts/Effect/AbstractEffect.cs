3e1fc0eeee7837b20f9673b4e65a0f7bc69902e5
roman.karimov@finch-vr.com <roman.karimov@finch-vr.com>
2022-06-20 20:18:54 +0500

    add summary

--- Assets/Scripts/Effect/AbstractEffect.cs
+++ Assets/Scripts/Effect/AbstractEffect.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Effect that apply to card
+/// </summary>
 public abstract class AbstractEffect
 {
     public delegate void ApplyEffect(AbstractCard card);
